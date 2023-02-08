038e990c33394224616607f1959e4580bd234c68
Yan de Carvalho Silva <yan.silva@aliare.co>
2023-02-08 17:39:47 -0300

    fit: header - colors/alignments

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 const menuLandscape = document.querySelector('.btn-group-vertical')
 menuLandscape.classList.remove('btn-group-vertical')
 menuLandscape.classList.add('btn-group-horizontal')
+
+// Header - Colors - Alignments
+const headerChanges = document.querySelector('.jumbotron')
+headerChanges.classList.add('bg-secondary', 'text-white', 'text-right')
+const headerBtnChanges = headerChanges.querySelector('a.btn')
+headerBtnChanges.classList.remove('btn-primary')
+headerBtnChanges.classList.add('btn-success')
