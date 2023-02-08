e42a6c06dbf9e15cc8f481558bea52c6a00070ec
Yan de Carvalho Silva <yan.silva@aliare.co>
2023-02-08 17:49:55 -0300

    fit: cards - order - btn color

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ headerChanges.classList.add('bg-secondary', 'text-white', 'text-right')
 const headerBtnChanges = headerChanges.querySelector('a.btn')
 headerBtnChanges.classList.remove('btn-primary')
 headerBtnChanges.classList.add('btn-success')
+
+// Cards
+// Order
+const cardChanges = document.querySelectorAll('.col-lg-3')
+cardChanges[0].classList.add('order-2')
+cardChanges[1].classList.add('order-4')
+cardChanges[2].classList.add('order-3')
+cardChanges[3].classList.add('order-1')
+// Btn Color
+const cardAnimal = cardChanges[0].querySelector('a.btn')
+cardAnimal.classList.remove('btn-primary')
+cardAnimal.classList.add('btn-success')
