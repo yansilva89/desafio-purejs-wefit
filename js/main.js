032eb36ea97a9340b7c7fa3fcd294602c52afeda
Yan de Carvalho Silva <yan.silva@aliare.co>
2023-02-08 17:24:49 -0300

    fit: Menu - Vertical to Landscape

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Menu - Vertical -> Horizontal
+const menuLandscape = document.querySelector('.btn-group-vertical')
+menuLandscape.classList.remove('btn-group-vertical')
+menuLandscape.classList.add('btn-group-horizontal')
