7005dc9cd508f3ff70b5fe81469ba96676556176
Yan de Carvalho Silva <yan.silva@aliare.co>
2023-02-08 18:10:50 -0300

    fit: lista - adding items

--- js/main.js
+++ js/main.js
@@ -21,3 +21,20 @@ cardChanges[3].classList.add('order-1')
 const cardAnimal = cardChanges[0].querySelector('a.btn')
 cardAnimal.classList.remove('btn-primary')
 cardAnimal.classList.add('btn-success')
+
+// Lista
+const list = document.querySelector('.list-group')
+//
+const fourthItem = document.createElement('li')
+fourthItem.classList.add('list-group-item', 'active')
+fourthItem.innerText = 'Quarto item'
+//
+const fifthItem = document.createElement('li')
+fifthItem.classList.add('list-group-item')
+fifthItem.innerText = 'Quinto item'
+//
+const firstItem = document.querySelector('.list-group-item.active')
+firstItem.classList.remove('active')
+//
+list.appendChild(fourthItem)
+list.appendChild(fifthItem)
